a73c7775ec0b55eb1aa2dd2f9aa14d397ecc4656
d10000usd <d10000usd@gmail.com>
2023-09-24 12:45:51 +0900

    Init

--- git_push_make_repository.sh
+++ git_push_make_repository.sh
@@ -11,17 +11,17 @@ dir_name=`basename $(pwd)`
 if [ "$repo_name" = "" ]; then
   repo_name=$dir_name
   fi
-  # 깃헙 계정 정보를 넣어주세요.
-  export GITHUB_USERNAME="d10000usd"
-  export GITHUB_EMAIL="d10000usd@gmail.com"
-  git config --global user.name "${GITHUB_USERNAME}"
-  git config --global user.email "${GITHUB_EMAIL}"
-  git config --global credential.helper cache
-  git config --global credential.helper 'cache --timeout=3600'
-  echo "MAKE REPO IN GITHUB"
-  curl -u "${GITHUB_USERNAME}" https://api.github.com/user/repos -d "{\"name\":\"$repo_name\"}"
-  echo "[Info] Creating Local Git Repository ...."
-  git init
+  # # 깃헙 계정 정보를 넣어주세요.
+  # export GITHUB_USERNAME="d10000usd"
+  # export GITHUB_EMAIL="d10000usd@gmail.com"
+  # git config --global user.name "${GITHUB_USERNAME}"
+  # git config --global user.email "${GITHUB_EMAIL}"
+  # git config --global credential.helper cache
+  # git config --global credential.helper 'cache --timeout=3600'
+  # echo "MAKE REPO IN GITHUB"
+  # curl -u "${GITHUB_USERNAME}" https://api.github.com/user/repos -d "{\"name\":\"$repo_name\"}"
+  # echo "[Info] Creating Local Git Repository ...."
+  # git init
   git add .
   # 디폴트로 'Init'이 커밋 메세지로 들어가게 했어요.
   # 이 부분 또한 편의에 맞게 바꿔 주시면 됩니다!
