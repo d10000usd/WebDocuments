c3583467c8049431ec3dd21af0bee782b4364e16
d10000usd <d10000usd@gmail.com>
2023-10-16 09:23:44 +0900

    Init

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { createChart } from "lightweight-charts";
+
+// REPLACE EVERYTHING BELOW HERE
+function generateData() {
+  var res = [];
+  var time = new Date(Date.UTC(2018, 0, 1, 0, 0, 0, 0));
+  for (var i = 0; i < 500; ++i) {
+    res.push({
+      time: time.getTime() / 1000,
+      value: i
+    });
+
+    time.setUTCDate(time.getUTCDate() + 1);
+  }
+
+  return res;
+}
+
+var chart = createChart(document.getElementById("container"));
+
+var mainSeries = chart.addLineSeries({
+  priceFormat: {
+    minMove: 1,
+    precision: 0
+  }
+});
+
+mainSeries.setData(generateData());
